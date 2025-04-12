d3dd81521d76cdf20234f822ad78614a3262b2c2
Fortune <fortunefoluso26@gmail.com>
2025-04-12 23:32:58 +0100

    product.js file Used

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,8 @@
-const products = [ {
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  ratings: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  ratings: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095,
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: "Adults Plain Cotton T- Shirt - 2 Pack",
-  ratings: {
-    stars: 4.5,
-    count: 56,
-  },
-  priceCents: 799,
-} ];
 const productsGrid = document.querySelector( '.products-grid' )
-let productHtml = ''
+let productsHtml = ''
 
 products.forEach( ( product ) => {
-  productHtml +=
+  productsHtml +=
     `
         <div class="product-container">
         <div class="product-image-container">
@@ -39,9 +14,9 @@ products.forEach( ( product ) => {
         </div>
 
         <div class="product-rating-container">
-          <img class="product-rating-stars" src="images/ratings/rating-${product.ratings.stars * 10}.png">
+          <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png">
           <div class="product-rating-count link-primary">
-            ${product.ratings.count}
+            ${product.rating.count}
           </div>
         </div>
 
@@ -78,4 +53,4 @@ products.forEach( ( product ) => {
   `
 } );
 
-productsGrid.innerHTML = productHtml
+productsGrid.innerHTML = productsHtml
